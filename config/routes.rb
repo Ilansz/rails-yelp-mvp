caa8e1238469ccaab97681a330f4441dce655b51
ilan szenker <szenkerilan@gmail.com>
2019-06-18 12:46:40 +0300

    review

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'reviews/index'
+  get 'reviews/new'
+  get 'reviews/create'
+  resources :restaurants, only: [:index, :new, :show, :create] do
+    resources :reviews, only: [:index, :new, :create]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
